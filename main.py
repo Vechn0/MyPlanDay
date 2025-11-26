119b2632f1931c46b9d42e9bc8e46bf6a9710423
Artem Fedoseev <Artikul563@gmail.com>
2025-11-26 21:51:36 +0300

    сделал бэкенд входа в систему

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request,redirect,url_for
 
 from flask_sqlalchemy import SqlAlchemy, SQLAlchemy
+from telebot.apihelper import session
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -29,6 +32,49 @@ class Task(db.Model):
 
 with app.app_context():
     db.create_all()
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if User.query.filter_by(username=username).first():
+            return "Такой логин уже используется"
+        hash_password = generate_password_hash(password)
+
+        new_user = User(username=username,password=hash_password)
+        db.session.add(new_user)
+        db.session.commit()
+        session["user_id"] = new_user.id
+        return redirect(url_for("task"))
+    else:
+        return render_template("index.html")
+
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password,password):
+            session["user_id"] = user.id
+            return redirect(url_for("task"))
+        else:
+            return "Неверный логин или пароль"
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user_id",None)
+    return redirect(url_for("index"))
+
+
+
 
 
 
